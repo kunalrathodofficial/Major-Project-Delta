a922e71128cdecf2459708fa055a624c652a69c8
Kunal Rathod <kunalrathod11131@gmail.com>
2024-01-02 11:30:52 +0530

    new file:   .gitignore

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+// by default implement the username and password
+
+module.exports = mongoose.model("User", userSchema);
